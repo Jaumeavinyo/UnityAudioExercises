75c2e2426274328476d64521777e0af805fcd67e
FBarnes99 <ferranbarnes@gmail.com>
2020-05-11 20:08:39 +0200

    Updtae

---
+++ WAG_No_Sound/Assets/Scripts/DayNightSwitch.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayNightSwitch : MonoBehaviour
+{
+   AudioSource ASource;
+    public AudioClip day_audio;
+    public AudioClip night_audio;
+
+    public DayNightCycle sunScript;
+    public bool day;
+
+    void Awake()
+    {
+         
+    }
+
+    void OnEnable()
+    {
+       
+    }
+
+    void OnDisable()
+    {
+        
+    }
+
+    void NightSwitch()
+    {
+        if (!day)
+        {
+            ASource.clip = night_audio;
+
+        }
+        else if (day) {
+
+            ASource.clip = day_audio;
+
+        }
+
+    }
+
+ 
+}
